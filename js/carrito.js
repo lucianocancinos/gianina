97332741489cf187cea869a26abf820949f1ca85
Luciano Cancinos <lucianocancinos99@gmail.com>
2020-12-30 12:57:26 -0300

    carrito funcionando

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,5 @@
 function miFuncion(){
-    var probando = document.getElementsByClassName("car-logo")[0].style.color = "green";
+   // var probando = document.getElementsByClassName("car-logo")[0].style.color = "green";
     const diva = document.getElementById("alerta");
         const parrafo = document.createElement("p");
         parrafo.textContent="Producto añadido con exito!";
